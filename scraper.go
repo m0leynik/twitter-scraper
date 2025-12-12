cd7fbc8be82b2936dd61239235ffc0b17424b731
Mikhail <14397301+m0leynik@users.noreply.github.com>
2025-12-13 00:49:49 +0300

    Change default user agent

--- scraper.go
+++ scraper.go
@@ -50,7 +50,7 @@ const (
 
 // default http client timeout
 const DefaultClientTimeout = 10 * time.Second
-const DefaultUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
+const DefaultUserAgent = "TwitterAndroid"
 
 // New creates a Scraper object
 func New() *Scraper {
